fff58a1d893d201decf0ea0071c8101ffc6d91fb
lucasbentoDev-Ops <lucas.aut166@gmail.com>
2024-07-25 15:03:10 -0300

    Adicionando método que válida a entrada dos números para o cálculo.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -5,18 +5,16 @@
         Menu();
     }
     
-     static void Menu()
+    static void Menu()
     {
         Console.Clear();
         string titulo = @"█▓▒▒░░░ Calculadora ░░░▒▒▓█";
         string Menssagem = "\n1 - Soma\t2 - Subtração\t3 - Divisão\t4 - Multiplicação\t0 - Sair";
         Console.WriteLine(titulo.PadLeft(Menssagem.Length));
         Console.WriteLine("Escolha uma das operação abaixo:\n" + Menssagem + "\n");
-        
+       
         string escolhaDoUsuario = Console.ReadLine()!;
-        if(escolhaDoUsuario == string.Empty)
-                 Menu();
-         
+        
          switch (escolhaDoUsuario)
         {
             case "1":
@@ -43,58 +41,68 @@
       
     }
 
+    // Cada método abaixo, corresponde a sua respectiva operação matemática.
     static void Somar()
     {
-        Console.Clear(); // limpa a tela
-        Console.WriteLine("Digite o primeiro valor:");
-        double numero1 = double.Parse(Console.ReadLine()!);
-        Console.WriteLine("Digite o segundo valor:");
-        double numero2 = double.Parse(Console.ReadLine()!);
+        Console.Clear();
+        double numero1 = LerNumero("Digite o primeiro valor:");
+        double numero2 = LerNumero("Digite o segundo valor:");
         double resultado = numero1 + numero2;
-        Console.WriteLine($"A soma de {numero1} + {numero2} = " + resultado);
+        Console.WriteLine($"A soma de {numero1} + {numero2} = {resultado}");
         Console.ReadKey();
         Menu();
     }
 
     static void Subtrair()
     {
-        Console.Clear(); // limpa a tela
-        Console.WriteLine("Digite o primeiro valor:");
-        double numero1 = double.Parse(Console.ReadLine()!);
-        Console.WriteLine("Digite o segundo valor:");
-        double numero2 = double.Parse(Console.ReadLine()!);
+        Console.Clear();
+        double numero1 = LerNumero("Digite o primeiro valor:");
+        double numero2 = LerNumero("Digite o segundo valor:");
         double resultado = numero1 - numero2;
-        Console.WriteLine($"A diferença de {numero1} - {numero2} = " + resultado);
+        Console.WriteLine($"A diferença de {numero1} - {numero2} = {resultado}");
         Console.ReadKey();
         Menu();
     }
 
     static void Dividir()
     {
-        Console.Clear(); // limpa a tela
-        Console.WriteLine("Digite o primeiro valor:");
-        double numero1 = double.Parse(Console.ReadLine()!);
-        Console.WriteLine("Digite o segundo valor:");
-        double numero2 = double.Parse(Console.ReadLine()!);
-        double resultado = numero1 / numero2;
-        Console.WriteLine($"A divisão de {numero1} / {numero2} = " + resultado);
+        Console.Clear();
+        double numero1 = LerNumero("Digite o primeiro valor:");
+        double numero2 = LerNumero("Digite o segundo valor:");
+        if (numero2 == 0)
+        {
+            Console.WriteLine("Erro: Divisão por zero não é permitida.");
+        }
+        else
+        {
+            double resultado = numero1 / numero2;
+            Console.WriteLine($"A divisão de {numero1} / {numero2} = {resultado}");
+        }
         Console.ReadKey();
         Menu();
-
     }
 
     static void Multiplicar()
     {
-        Console.Clear(); // limpa a tela
-        Console.WriteLine("Digite o primeiro valor:");
-        double numero1 = double.Parse(Console.ReadLine()!);
-        Console.WriteLine("Digite o segundo valor:");
-        double numero2 = double.Parse(Console.ReadLine()!);
+        Console.Clear();
+        double numero1 = LerNumero("Digite o primeiro valor:");
+        double numero2 = LerNumero("Digite o segundo valor:");
         double resultado = numero1 * numero2;
-        Console.WriteLine($"A Multiplicação de {numero1} X {numero2} = " + resultado);
+        Console.WriteLine($"A multiplicação de {numero1} x {numero2} = {resultado}");
         Console.ReadKey();
         Menu();
+    }
+    
 
+    // Este método válida a entrada dos números para o cálculo.
+    static double LerNumero(string mensagem)
+    {
+        double numero;
+        Console.WriteLine(mensagem);
+        while (!double.TryParse(Console.ReadLine(), out numero))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um número válido.");
+        }
+        return numero;
     }
 }
-
